30a49158888fa21cce7327ce80c45cb32eab7cd3
BrindhaKavinKumar <brindha.b2501@gmail.com>
2026-03-11 10:35:02 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,8 +58,8 @@ resource "aws_security_group" "sg" {
 
   ingress {
     description = "Jenkins"
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 8081
+    to_port     = 8081
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
